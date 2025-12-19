af0dd1e194af1e875e04f9de314e98cbcf290fe5
imbhuvi1 <bhuvneshsingh292@gmail.com>
2025-12-19 11:57:22 +0530

    Resolved merge conflicts

---
+++ src/com/bridgelabz/gcrcodebase/programmingelements/assistedproblems/AddTwoNumbers.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.gcrcodebase.programmingelements.assistedproblems;
+
+import java.util.*;
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		//Taking input of two numbers
+		int firstNumber = sc.nextInt();
+		int secondNumber = sc.nextInt();
+		
+		//adding two numbers
+		int sumOfTwoNumbers = firstNumber + secondNumber;
+		
+		//printing numbers
+		System.out.println("Sum of Two Numbers: "+sumOfTwoNumbers);
+		
+
+	}
+
+}
