83532611513405e7c91512bac29a5b542fa75491
imbhuvi1 <bhuvneshsingh292@gmail.com>
2025-12-20 14:08:30 +0530

    [Bhuvnesh]: chore. deleted DummyCode.java file in sceneriobased

--- corejavapractice/sceneriobased/DummyCode.java
+++
@@ -1,9 +0,0 @@
-package sceneriobased;
-
-public class DummyCode {
-
-	public static void main(String[] args) {
-		System.out.println("It's a dummy file that will be deleted later. Just to store the package non-empty do it can be stoted remotely");
-	}
-
-}
