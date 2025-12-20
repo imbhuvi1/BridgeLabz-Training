a4beb67ad59853b12d3d7f5c61ff24db18f5c802
imbhuvi1 <bhuvneshsingh292@gmail.com>
2025-12-20 15:17:17 +0530

    [Bhuvnesh Singh Bhadauriya]: Feat .Solved CoffeeCounterChrnociles problem of Scenerio Based

--- corejavapractice/sceneriobased/CoffeeCounterChronicles.java
+++ corejavapractice/sceneriobased/CoffeeCounterChronicles.java
@@ -1,10 +1,56 @@
 package sceneriobased;
 
+import java.util.*;
 public class CoffeeCounterChronicles {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		final double GST_RATE = 0.05; //5% GST;
+		
+		while(true) {
+			System.out.println("Enter coffee type (Espresso/Latte/Cappuccino) or 'exit' to stop: ");
+			String coffeeType = sc.nextLine();
+			
+			if(coffeeType.equalsIgnoreCase("exit")) {
+				System.out.println("Thank you for visiting Ravi's Cafe");
+                break;
+			}
+			
+			System.out.println("Enter quantity: ");
+			int quantity = sc.nextInt();
+			sc.nextLine();
+			
+			double price;
+			
+			switch(coffeeType.toLowerCase()) {
+			case "espresso":
+				price = 120;
+				break;
+	
+			case "latte":
+				price = 120;
+				break;
+			
+			case "cappucino":
+				price = 120;
+				break;
+				
+			default:
+				System.out.println("Invalid coffee type!");
+                continue;
+			}
+			
+			//calculating the bill now
+			double total = price * quantity;
+			double gst = total * GST_RATE;
+			double finalBill = total + gst;
+			
+			System.out.println("Base Amount: Rs." + total);
+            System.out.println("GST (5%): Rs." + gst);
+            System.out.println("Total Bill: Rs." + finalBill);
+		}
+		
+		
 	}
 
 }
