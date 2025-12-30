fbd8f637fa0dce229742b8a191b27581772762f8
imbhuvi1 <bhuvneshsingh292@gmail.com>
2025-12-30 23:09:13 +0530

    [Bhuvnesh Singh Bhadauriya] Refactor. changed the code according to the instructions given later

--- core-java-practice/scenerio-based/EmployeeWageComputation.java
+++ core-java-practice/scenerio-based/EmployeeWageComputation.java
@@ -1,74 +1,299 @@
+class EMC_UC1 {
 
-import java.util.ArrayList;
+	// UC-1 Check if Employee is Present Or Absent using Random()
+	public int getAttendance() {
+		return (int) (Math.random() * 3);
+	}
+}
 
-class Employee {
+class EWC_UC2 {
+	
+	// UC-1 Check if Employee is Present Or Absent using Random()
+	public int getAttendance() {
+		return (int) (Math.random() * 3);
+	}
+	
+	// UC-2 Full-Time Employee Daily Wage Calculation
+	public int getDailyWage() {
+		final int FULL_TIME_HOURS = 8;
+		final int WAGE_PER_HOUR = 20;
+		
+		int attendance = getAttendance();
+		int dailyHours = 0;
+		
+		if(attendance > 0) {
+			dailyHours = FULL_TIME_HOURS;
+		}
 
- static final int WAGE_PER_HOUR = 20;
- static final int MAX_WORKING_DAYS = 20;
- static final int MAX_WORKING_HOURS = 100;
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
+}
 
- int empId;
- int totalWorkingHours;
- int totalWorkingDays;
+class EWC_UC3 {
+	
+	// UC-1 Check if Employee is Present Or Absent using Random()
+	public int getAttendance() {
+		return (int) (Math.random() * 3);
+	}
+	
+	// UC-3 Part-Time Employee Wage Calculation
+	public int getDailyWage() {
+		final int PART_TIME_HOURS = 4;
+		final int WAGE_PER_HOUR = 20;
+		
+		int attendance = getAttendance();
+		int dailyHours = 0;
+		
+		if(attendance > 0) {
+			dailyHours = PART_TIME_HOURS;
+		}
 
- Employee(int empId) {
-     this.empId = empId;
- }
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
+}
 
- int calculateWage() {
-     return totalWorkingHours * WAGE_PER_HOUR;
- }
+class EWC_UC4 {
+	// UC-1 Check if Employee is Present Or Absent using Random()
+	public int getAttendance() {
+		return (int) (Math.random() * 3);
+	}
 
- void computeMonthlyWage() {
+	// UC-2 Full-Time Employee Daily Wage Calculation
+	public int getDailyWage() {
+		final int FULL_TIME_HOURS = 8;
+		final int WAGE_PER_HOUR = 20;
+		
+		int attendance = getAttendance();
+		int dailyHours = 0;
+		
+		if(attendance > 0) {
+			dailyHours = FULL_TIME_HOURS;
+		}
 
-     while (totalWorkingHours < MAX_WORKING_HOURS &&
-            totalWorkingDays < MAX_WORKING_DAYS) {
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
 
-         totalWorkingDays++;
+	// UC-3 Part-Time Employee Wage Calculation
+	public int getPartTimeDailyWage() {
+		final int PART_TIME_HOURS = 4;
+		final int WAGE_PER_HOUR = 20;
+		
+		int attendance = getAttendance();
+		int dailyHours = 0;
+		
+		if(attendance > 0) {
+			dailyHours = PART_TIME_HOURS;
+		}
 
-         int empCheck = (int) (Math.random() * 3);
-         int empHours = 0;
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
 
-         switch (empCheck) {
-             case 1: 
-                 empHours = 8;
-                 break;
+	// UC-4 Solving Using Switch Case
+	public int getDailyHours(int attendance) {
+		final int FULL_TIME_HOURS = 8;
+		final int PART_TIME_HOURS = 4;
+		int dailyHours = 0;
+		switch (attendance) {
+			case 1 -> {
+				dailyHours = FULL_TIME_HOURS;
+			}
+			case 2 -> {
+				dailyHours = PART_TIME_HOURS;
+			}
+			default -> {
+				dailyHours = 0;
+			}
+		}
+		return dailyHours;
+	}
+}
 
-             case 2: 
-                 empHours = 8;
-                 break;
+class UC5 {
+	// UC-1 Check if Employee is Present Or Absent using Random()
+	public int getAttendance() {
+		return (int) (Math.random() * 3);
+	}
 
-             default:
-                 empHours = 0;
-         }
+	// UC-2 Full-Time Employee Daily Wage Calculation
+	public int getDailyWage(int dailyHours) {
+		final int WAGE_PER_HOUR = 20;
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
 
-         totalWorkingHours += empHours;
-     }
- }
+	// UC-3 Part-Time Employee Wage Calculation
+	public int getPartTimeDailyWage(int dailyHours) {
+		final int WAGE_PER_HOUR = 20;
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
+
+	// UC-4 Solving Using Switch Case
+	public int getDailyHours(int attendance) {
+		final int FULL_TIME_HOURS = 8;
+		final int PART_TIME_HOURS = 4;
+		int dailyHours = 0;
+		switch (attendance) {
+			case 1 -> {
+				dailyHours = FULL_TIME_HOURS;
+			}
+			case 2 -> {
+				dailyHours = PART_TIME_HOURS;
+			}
+			default -> {
+				dailyHours = 0;
+			}
+		}
+		return dailyHours;
+	}
+	//UC-5 	Calculate Wages for a month
+	public int getMonthlyWage() {
+		int totalWage = 0;
+		int totalWorkingDays = 0;
+		while(totalWorkingDays < 20) {
+			totalWorkingDays++;
+			int attendance = getAttendance();
+			int dailyHours = getDailyHours(attendance);
+			int dailyWage = getDailyWage(dailyHours);
+			totalWage += dailyWage;
+		}
+		return totalWage;
+	}
+}
+
+class EWC_UC6{
+	// UC-1 Check if Employee is Present Or Absent using Random()
+	public int getAttendance() {
+		return (int) (Math.random() * 3);
+	}
+
+	// UC-2 Full-Time Employee Daily Wage Calculation
+	public int getDailyWage(int dailyHours) {
+		final int WAGE_PER_HOUR = 20;
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
+
+	// UC-3 Part-Time Employee Wage Calculation
+	public int getPartTimeDailyWage(int dailyHours) {
+		final int WAGE_PER_HOUR = 20;
+		int dailyWage = dailyHours * WAGE_PER_HOUR;
+		return dailyWage;
+	}
+
+	// UC-4 Solving Using Switch Case
+	public int getDailyHours(int attendance, int totalWorkingHours) {
+		final int FULL_TIME_HOURS = 8;
+		final int PART_TIME_HOURS = 4;
+		final int MAX_WORKING_HOURS = 100;
+		int dailyHours = 0;
+		switch (attendance) {
+			case 1 -> {
+				dailyHours = FULL_TIME_HOURS;
+			}
+			case 2 -> {
+				dailyHours = PART_TIME_HOURS;
+			}
+			default -> {
+				dailyHours = 0;
+			}
+		}
+		if (totalWorkingHours + dailyHours > MAX_WORKING_HOURS) {
+			dailyHours = MAX_WORKING_HOURS - totalWorkingHours;
+		}
+
+		return dailyHours;
+	}
+	
+	// UC-6 Calculate wages till total working hours or total working days condition is reached
+	public int getMonthlyWage() {
+		final int MAX_WORKING_DAYS = 20;
+		final int MAX_WORKING_HOURS = 8;
+		int totalWage = 0;
+		int totalWorkingDays = 0;
+		int totalWorkingHours = 0;
+		while(totalWorkingHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
+			totalWorkingDays++;
+			int attendance = getAttendance();
+			int dailyHours = getDailyHours(attendance, totalWorkingHours);
+			totalWorkingHours += dailyHours;
+			int dailyWage = attendance == 2 ? getPartTimeDailyWage(dailyHours) : getDailyWage(dailyHours);
+			totalWage += dailyWage;
+		}
+		return totalWage;
+	}
+	
 }
 
 public class EmployeeWageComputation {
 
- static void displayWelcomeMessage() {
-     System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
- }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		System.out.println();
 
- public static void main(String[] args) {
+		EmployeeWageComputation obj = new EmployeeWageComputation();
 
-     displayWelcomeMessage();
+		final int WAGE_PER_HOUR = 20;
+		final int FULL_TIME_HOURS = 8;
+		final int PART_TIME_HOURS = 4;
+		final int MAX_WORKING_DAYS = 20;
+		final int MAX_WORKING_HOURS = 100;
 
-     ArrayList<Employee> employeeList = new ArrayList<>();
+		int totalWorkingHours = 0;
+		int totalWorkingDays = 0;
+		int totalWage = 0;
 
-     employeeList.add(new Employee(101));
-     employeeList.add(new Employee(102));
-     employeeList.add(new Employee(103));
+		// UC-6 Calculate wages till total working hours or total working days condition is reached
+		while (totalWorkingHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
+			totalWorkingDays++;
 
-     for (Employee emp : employeeList) {
-         emp.computeMonthlyWage();
-         System.out.println("Employee ID          : " + emp.empId);
-         System.out.println("Total Working Days   : " + emp.totalWorkingDays);
-         System.out.println("Total Working Hours  : " + emp.totalWorkingHours);
-         System.out.println("Total Monthly Wage   : ₹" + emp.calculateWage());
-     }
- }
+			// UC-1 Check if Employee is Present Or Absent using Random()
+			// 0 for absent, 1 for pull time present, 2 for part time present
+			int attendance = (int)(Math.random() * 3);
+			int dailyHours = 0;
+
+			// UC-4 Solving Using Switch Case
+			switch (attendance) {
+			// UC-2 Full-Time Employee Daily Wage Calculation
+				case 1 -> {
+					dailyHours = FULL_TIME_HOURS;
+					System.out.println("Day " + totalWorkingDays + " : Full Time Present");
+				}
+				// UC-3 Part-Time Employee Wage Calculation
+				case 2 -> {
+					dailyHours = PART_TIME_HOURS;
+					System.out.println("Day " + totalWorkingDays + " : Part Time Present");
+				}
+				default -> {
+					System.out.println("Day " + totalWorkingDays + " : Absent");
+				}
+			}
+
+			// UC-6 Ensuring Total Hours Do Not Exceed Max Working Hours
+			if (totalWorkingHours + dailyHours > MAX_WORKING_HOURS) {
+				dailyHours = MAX_WORKING_HOURS - totalWorkingHours;
+			}
+
+			totalWorkingHours += dailyHours;
+
+			// UC-2 Daily Wage Calculation
+			int dailyWage = dailyHours * WAGE_PER_HOUR;
+
+			// UC-5 Calculating Wages For A Month
+			totalWage += dailyWage;
+
+			System.out.println(
+					"Day " + totalWorkingDays + " : Daily Hours : " + dailyHours + "\nDaily Wage : " + dailyWage);
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("Total Working Days : " + totalWorkingDays);
+		System.out.println("Total Working Hours : " + totalWorkingHours);
+		System.out.println("Total Monthly Wage : Rs " + totalWage);
+	}
 }
