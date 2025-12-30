3ebcb40ef08c11d213ae1d88a6d6d1b1f9660744
imbhuvi1 <bhuvneshsingh292@gmail.com>
2025-12-31 00:33:29 +0530

    [Bhuvnesh Singh Bhadauriya] Fixed. Removed a fie that was unnecessary

--- remove_package.ps1
+++
@@ -1,7 +0,0 @@
-Get-ChildItem -Path 'core-java-practice' -Recurse -Filter '*.java' | ForEach-Object {
-    $content = Get-Content $_.FullName
-    if ($content[0] -match '^package\s+.*;$') {
-        $newContent = $content | Select-Object -Skip 1
-        Set-Content $_.FullName $newContent
-    }
-}
