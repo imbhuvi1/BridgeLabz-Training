07c26902af550701c8fe536a95ef28e3813887c2
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-01-21 16:08:13 +0530

    [Bhuvnesh Singh Bhadauriya] Feat. Solved the assignment problems of GENERICS

---
+++ collections-practice/gcr-codebase/generics/MealPlanner/Meal.java
@@ -0,0 +1,24 @@
+package gcr_codebase.generics.MealPlanner;
+
+
+public class Meal<T extends MealPlan> {
+    private String mealName;
+    private T mealPlanType;
+
+    public Meal(String mealName, T mealPlanType) {
+        this.mealName = mealName;
+        this.mealPlanType = mealPlanType;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public T getMealPlanType() {
+        return mealPlanType;
+    }
+
+    public void displayMeal() {
+        System.out.println("Meal: " + mealName + ", Type: " + mealPlanType.getMealType() + ", Description: " + mealPlanType.getDescription());
+    }
+}
