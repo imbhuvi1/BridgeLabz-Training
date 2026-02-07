59989ab82f118f38538d0c7937207d129df1b9c1
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-02-07 15:33:23 +0530

    [Bhuvnesh Singh Bhadauriya] Deleted. removed copied files in new branch

--- collections-practice/gcr-codebase/generics/DynamicOnlineMarketPlace/BookCategory.java
+++
@@ -1,8 +0,0 @@
-package gcr_codebase.generics.DynamicOnlineMarketplace;
-
-class BookCategory implements ProductCategory {
-    @Override
-    public String getCategoryName() {
-        return "Books";
-    }
-}
--- collections-practice/gcr-codebase/generics/MealPlanner/MealPlan.java
+++
@@ -1,8 +0,0 @@
-package gcr_codebase.generics.MealPlanner;
-
-
-public interface MealPlan {
-    String getMealType();
-    String getDescription();
-    void prepareMeal();
-}
