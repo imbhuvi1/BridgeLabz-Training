170420e0dca569af51f6111caa5bc2a982de6859
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-02-26 13:13:35 +0530

    UC1: Created contact with personal details

---
+++ oops-practice/scenario-based/employee-wage/adress-book/uc1/Contact.java
@@ -0,0 +1,25 @@
+package adress_book.uc1;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
