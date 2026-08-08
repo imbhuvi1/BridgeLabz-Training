99e6ef72fa5bf2a65a661148e69ccef22b1eedb8
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-08-08 18:49:30 +0530

    [Bhuvnesh] : Feat. Hello with name in query parameter

--- Day-6/firstspringapp/src/main/java/com/firstspringapp/controller/HelloWorldController.java
+++ Day-6/firstspringapp/src/main/java/com/firstspringapp/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.firstspringapp.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World !";
+    }
+
+    @GetMapping("/hello/query")
+    public String helloName(@RequestParam String name){
+        return "Hello "+name;
     }
 
 }
