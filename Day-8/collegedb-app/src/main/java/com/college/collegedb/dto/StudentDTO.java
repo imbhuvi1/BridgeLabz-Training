2facd76060abf6c17820dbdd1dc38f0a8bcc45ab
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-08-16 21:54:14 +0530

    [Bhuvnesh] : Feat. Completing the dto module

--- Day-8/collegedb-app/src/main/java/com/college/collegedb/dto/StudentDTO.java
+++ Day-8/collegedb-app/src/main/java/com/college/collegedb/dto/StudentDTO.java
@@ -1,129 +1,22 @@
 package com.college.collegedb.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class StudentDTO {
-
     private Integer studentId;
-
-    @NotBlank(message = "Roll number is required")
     private String rollNumber;
-
-    @NotBlank(message = "First name is required")
     private String firstName;
-
     private String lastName;
     private String gender;
-
-    @Email(message = "Email should be valid")
     private String email;
-
     private String address;
     private String city;
     private String state;
     private String course;
     private String department;
     private Integer admissionYear;
-
-    public StudentDTO() {
-    }
-
-    // Getters and Setters
-
-    public Integer getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(Integer studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getRollNumber() {
-        return rollNumber;
-    }
-
-    public void setRollNumber(String rollNumber) {
-        this.rollNumber = rollNumber;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public Integer getAdmissionYear() {
-        return admissionYear;
-    }
-
-    public void setAdmissionYear(Integer admissionYear) {
-        this.admissionYear = admissionYear;
-    }
 }
